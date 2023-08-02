5c054b53c09d588eb15cbd8b60ca36cf6b8e0dbc
kim <1347408207@qq.com>
2023-08-02 11:05:46 +0800

    feat: prisma

---
+++ src/core/client.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: kim
+ * @Date: 2023-08-01 22:58:25
+ * @Description: 
+ */
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default prisma
